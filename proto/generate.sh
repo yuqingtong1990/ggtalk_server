248350e0ea8aecc2b475e96dbdf2f858659f85c4
yuqingtong <yuqingtong1990@gmail.com>
2018-11-09 14:59:09 +0800

    添加pb文件，和生产文件

---
+++ proto/generate.sh
@@ -0,0 +1,5 @@
+protoc --proto_path=. --cpp_out=../server/proto IMBaseDefine.proto
+protoc --proto_path=. --cpp_out=../server/proto IMCmdIdDefine.proto
+protoc --proto_path=. --cpp_out=../server/proto IMFriendPackage.proto
+protoc --proto_path=. --cpp_out=../server/proto IMGroupPackage.proto
+protoc --proto_path=. --cpp_out=../server/proto IMMsgPackage.proto
